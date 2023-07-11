2468a9bb6e26c127893c686fcc57db6a6320cf94
rajat <32998741+rajatdiptabiswas@users.noreply.github.com>
2023-07-11 11:50:55 -0700

    Add `decoy/XRelay.py` logging

--- decoy/XRelay.py
+++ decoy/XRelay.py
@@ -1,4 +1,4 @@
-__author__ = 'milad'
+__author__ = "milad"
 
 import datetime
 import socket
@@ -12,7 +12,8 @@ import threading
 import base64
 import SocketServer
 import fcntl
-serv_addr=('localhost',8979)
+
+serv_addr = ("localhost", 8979)
 
 import os
 from ServerConnection import ProxyServer
@@ -20,70 +21,76 @@ import struct
 import errno
 
 
-class RelayManager():
-    def __init__(self):
+import logging
 
+log = logging.getLogger(__name__)
+
+
+class RelayManager:
+    def __init__(self):
         self.server = ProxyServer(serv_addr)
-        self.serverconnectionthread = threading.Thread(target=self.server.run).start()
+        self.serverconnectionthread = threading.Thread(
+            target=self.server.run
+        ).start()
 
         self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.conn.connect(serv_addr)
-        #fcntl.fcntl(self.conn, fcntl.F_SETFL, os.O_NONBLOCK)
-        self.datacarry=''
-        self.headersize=37
-        self.connections={}
-        self.sendlock=threading.Lock()
-        self.recvlock=threading.Lock()
+        # fcntl.fcntl(self.conn, fcntl.F_SETFL, os.O_NONBLOCK)
+        self.datacarry = ""
+        self.headersize = 37
+        self.connections = {}
+        self.sendlock = threading.Lock()
+        self.recvlock = threading.Lock()
 
-
-    def processCMD(self,pkt,connid):
+    def processCMD(self, pkt, connid):
         with self.sendlock:
-            self.conn.sendall(str(connid)+pkt)
+            self.conn.sendall(str(connid) + pkt)
 
+    def processDATA(self, data):
+        self.datacarry += data
 
-    def processDATA(self,data):
-        self.datacarry+=data
-
-        flag=True
+        flag = True
         while flag:
-            if len(self.datacarry)<self.headersize:
+            if len(self.datacarry) < self.headersize:
                 break
-            connid=self.datacarry[:16]
-            size=struct.unpack('>I', self.datacarry[33:37])[0]
-            if size+ self.headersize <= len(self.datacarry):
-                self.connections.setdefault(connid,[]).append(self.datacarry[16:size+ self.headersize ])
-                if size+ self.headersize == len(self.datacarry):
-                    self.datacarry=''
-                    flag=False
+            connid = self.datacarry[:16]
+            size = struct.unpack(">I", self.datacarry[33:37])[0]
+            if size + self.headersize <= len(self.datacarry):
+                self.connections.setdefault(connid, []).append(
+                    self.datacarry[16 : size + self.headersize]
+                )
+                if size + self.headersize == len(self.datacarry):
+                    self.datacarry = ""
+                    flag = False
                 else:
-                    self.datacarry=self.datacarry[size+ self.headersize:]
+                    self.datacarry = self.datacarry[size + self.headersize :]
 
             else:
-                flag=False
-
+                flag = False
 
     def autofetch(self):
         while True:
-            r,w,e=select.select([self.conn],[],[])
-            if r :
+            r, w, e = select.select([self.conn], [], [])
+            if r:
                 msg = self.conn.recv(4096)
-                print 'get data from serverconnection',datetime.datetime.now()
+                log.debug(
+                    "Get data from ServerConnection {}".format(
+                        datetime.datetime.now()
+                    )
+                )
                 self.processDATA(msg)
 
-
-    def getnewpackets(self,connid):
-        return self.connections.pop(connid,[])
-
+    def getnewpackets(self, connid):
+        return self.connections.pop(connid, [])
 
 
-
-class Relay():
-    _manager=None
+class Relay:
+    _manager = None
 
     def __init__(self):
-        if Relay._manager==None:
-            Relay._manager= RelayManager()
+        if Relay._manager == None:
+            Relay._manager = RelayManager()
             threading.Thread(target=Relay._manager.autofetch).start()
 
     def __getattr__(self, item):
