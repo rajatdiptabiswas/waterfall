b6e418cb3de99c4e82a9e5ee6c80f5e1a5589e6e
rajat <32998741+rajatdiptabiswas@users.noreply.github.com>
2024-09-08 16:46:19 -0700

    Update delete-latest-logs.sh

--- client/delete-latest-logs.sh
+++ client/delete-latest-logs.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-
-# List of directories where you want to delete the latest logs
 directories=(
     "./captures"
     "./logs"
 )
 
-# Iterate through the directories
-for dir in "${directories[@]}"; do
-    # Check if the directory exists
-    if [ ! -d "$dir" ]; then
-        echo "Directory not found: $dir"
-        continue  # Move to the next directory if this one doesn't exist
-    fi
+latest_timestamp=$(find ${directories[@]} -type f -printf "%T+ %p\n" | sort -r | head -n 1 | cut -d' ' -f2- | grep -oP '\d{14}')
 
-    # Change to the directory
-    cd "$dir" || continue  # Move to the next directory if CD fails
+if [ -z "$latest_timestamp" ]; then
+    echo "Latest timestamp extraction failed"
+    exit 1
+fi
 
-    # List files in the directory by modification time, with the latest first
-    latest_file=$(ls -t | head -n 1)
+echo "The following files will be deleted"
+find ${directories[@]} -type f -name "*$latest_timestamp*"
 
-    # Check if a file was found
-    if [ -z "$latest" ]; then
-        echo "No files found in $dir."
-    else
-        # Delete the latest file
-        rm "$latest"
-        echo "Deleted the latest file in $dir: $latest"
-    fi
-done
+read -p "Are you sure you want to delete these files? (y/n) " confirmation
 
+if [[ $confirmation =~ ^[Yy]$ ]]; then
+    find "${directories[@]}" -type f -name "*$latest_timestamp*" -print0 | xargs -0 rm -v
+    echo "Files have been deleted"
+else
+    echo "File deletion cancelled"
+    exit 0
+fi
