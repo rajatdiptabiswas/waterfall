5b10a609fe9d09577a5d75b77e4e24f5c11bcf21
rajat <rdb5@sfu.ca>
2024-09-08 16:00:26 -0700

    Update tmux-experiment-setup.sh

--- scripts/client/tmux-experiment-setup.sh
+++ scripts/client/tmux-experiment-setup.sh
@@ -9,7 +9,7 @@ sleep_countdown() {
     fi
 
     echo "Waiting $seconds seconds..."
-
+    
     for (( i=seconds; i>=1; i-- )) do
         printf "\r%s\033[K" "$i"
         sleep 1
@@ -29,28 +29,26 @@ tmux_send_keys() {
 }
 
 
-echo "Starting decoy tmux terminal window"
-tmux new-session -d -s experiment
+echo "Starting client|decoy tmux terminal window"
+tmux new-session -d -s waterfall
+tmux split-window -h -t waterfall:0
 
-tmux_send_keys experiment:0 "cd ~" 1
+echo "Starting decoy setup"
+tmux_send_keys waterfall:0.1 "cd ~" 2
 echo "SSH into decoy VM"
-tmux_send_keys experiment:0 "./ssh-decoy.sh" 2
+tmux_send_keys waterfall:0.1 "./ssh-decoy.sh" 5
 echo "Getting superuser access"
-tmux_send_keys experiment:0 "su" 2
-tmux_send_keys experiment:0 "waterfall" 2
-echo "Starting decoy setup script"
-tmux_send_keys experiment:0 ". setup.sh" 0
+tmux_send_keys waterfall:0.1 "su" 5
+tmux_send_keys waterfall:0.1 "waterfall" 5
+echo "Running decoy setup script"
+tmux_send_keys waterfall:0.1 ". setup.sh" 5
 
-
-echo "Starting client tmux terminal window"
-tmux split-window -h -t experiment:0
-
-tmux_send_keys experiment:0 "cd ~" 1
+echo "Starting client setup"
+tmux_send_keys waterfall:0.0 "cd ~" 2
 echo "Getting superuser access"
-tmux_send_keys experiment:0 "su" 2
-tmux_send_keys experiment:0 "waterfall" 2
+tmux_send_keys waterfall:0.0 "su" 5
+tmux_send_keys waterfall:0.0 "waterfall" 5
 echo "Running client setup script"
-tmux_send_keys experiment:0 ". setup.sh" 0
-
+tmux_send_keys waterfall:0.0 ". setup.sh" 5
 
 tmux attach
