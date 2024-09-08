eada6f16831ad9c4186abe35f416aba88eaf781b
rajat <32998741+rajatdiptabiswas@users.noreply.github.com>
2024-09-08 16:37:11 -0700

    Update experiment.sh

--- client/experiment.sh
+++ client/experiment.sh
@@ -10,9 +10,9 @@ number_of_runs="$1"
 for ((i = 1; i <= number_of_runs; i++)); do
     echo "RUNNING EXPERIMENT $i..."
     
-    tmux send-keys -t 0 "./decoy-experiment.sh" C-m 
+    tmux send-keys -t waterfall:0.1 "./decoy-experiment.sh" C-m 
     bash "./client-experiment.sh"
-    tmux send-keys -t 0 C-c
+    tmux send-keys -t waterfall:0.1 C-c
 
     echo "COMPLETED EXPERIMENT $i"
 done
